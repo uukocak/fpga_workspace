a2b6285ecad30c9fda9b136f1f8d051d778f8eaa
Sümeyra Durak <sumeyra.durak@esensi.local>
2018-08-17 17:23:47 +0300

    Update lcd_little_wrapper.vhd

--- lcd/lcd_little_wrapper.vhd
+++ lcd/lcd_little_wrapper.vhd
@@ -27,11 +27,13 @@ architecture behavioral of lcd_little_wrapper is
 
 
   type state_t is (idle_st, reset_st, write_st);
+  type vector_array_t is array (0 to 15) of std_logic_vector(9 downto 0);
 
   signal lcd_enable    : std_logic;
-  signal lcd_bus       : std_logic_vector(9 downto 0);
+  signal i_lcd_bus     : std_logic_vector(9 downto 0);
   signal busy          : std_logic;
-  signal current_state : state_t;
+  signal current_state : state_t := reset_st;
+  signal my_word       : vector_array_t;
 
 begin
 
@@ -40,15 +42,55 @@ port map (
   clk        => clk,
   reset_n    => reset_n,
   lcd_enable => lcd_enable,
-  lcd_bus    => lcd_bus,
+  lcd_bus    => i_lcd_bus,
   busy       => busy,
   e          => e,
   lcd_data   => lcd_data
 );
 
-state_pro : process(sensitivity_list)
+state_pro : process(current_state,clk,reset_n)
+    VARIABLE clk_count : INTEGER := 0;
+    VARIABLE word_index : INTEGER := 0;
 begin
 
+    if( reset_n = '1') then
+        current_state <= reset_st;
+    end if;
+
+    case( current_state ) is
+
+        when reset_st =>
+            if( clk_count < 50000) then
+                clk_count := clk_count + 1;
+            else
+                clk_count := 0;
+                current_state <= idle_st;
+            end if;
+
+        when idle_st =>
+            if( busy = '0' and word_index < 16 ) then
+                i_lcd_bus <= my_word(word_index);
+                lcd_enable <= '1';
+                current_state <= write_st;
+            else
+                lcd_enable <= '0';
+                current_state <= idle_st;
+            end if;
+
+        when write_st =>
+            if( clk_count < 1) then
+                clk_count := clk_count + 1;
+                current_state <= write_st;
+            else
+                lcd_enable <= '0';
+                word_index := word_index + 1;
+                current_state <= idle_st;
+            end if;
+
+        when others =>
+            current_state <= idle_st ;
+
+    end case;
 
 end process;
 
