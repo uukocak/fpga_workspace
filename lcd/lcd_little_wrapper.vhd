06123fd5e881ac16e96622eff19ee9d8dcb33d77
Umut Utku Kocak <umutkocak@hacettepe.edu.tr>
2018-08-28 10:04:12 +0300

    LCD update

--- lcd/lcd_little_wrapper.vhd
+++ lcd/lcd_little_wrapper.vhd
@@ -1,57 +1,61 @@
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
 entity lcd_little_wrapper is
-  port (
-  clk        : in    std_logic;  --system clock
-  reset_n    : in    std_logic;  --active low reinitializes lcd
-  rw, rs, e  : out   std_logic;  --read/write, setup/data, and enable for lcd
-  lcd_data   : out   std_logic_vector(3 downto 0)--data signals for lcd
-  );
+    port (
+    clk        : in    std_logic;  --system clock
+    reset_n    : in    std_logic;  --active low reinitializes lcd
+    rw, rs, e  : out   std_logic;  --read/write, setup/data, and enable for lcd
+    lcd_data   : out   std_logic_vector(3 downto 0)--data signals for lcd
+    );
 end lcd_little_wrapper;
 
 architecture behavioral of lcd_little_wrapper is
 
-  component lcd_controller
-  port (
-    clk        : in  std_logic;
-    reset_n    : in  std_logic;
-    lcd_enable : in  std_logic;
-    lcd_bus    : in  std_logic_vector(9 downto 0);
-    busy       : out std_logic;
-    e          : out std_logic;
-    lcd_data   : out std_logic_vector(3 downto 0)
-  );
-  end component lcd_controller;
+    component lcd_controller
+    	port (
+    		clk        : in  std_logic;
+    		reset_n    : in  std_logic;
+    		lcd_enable : in  std_logic;
+    		lcd_bus    : in  std_logic_vector(9 downto 0);
+    		busy       : out std_logic := '1';
+    		e          : out std_logic;
+    		rw         : out std_logic;
+    		rs         : out std_logic;
+    		lcd_data   : out std_logic_vector(3 downto 0));
+    end component;
 
+    type state_t is (idle_st, reset_st, write_st);
+    type vector_array_t is array (0 to 15) of std_logic_vector(9 downto 0);
 
-  type state_t is (idle_st, reset_st, write_st);
-  type vector_array_t is array (0 to 15) of std_logic_vector(9 downto 0);
-
-  signal lcd_enable    : std_logic;
-  signal i_lcd_bus     : std_logic_vector(9 downto 0);
-  signal busy          : std_logic;
-  signal current_state : state_t := reset_st;
-  signal my_word       : vector_array_t;
+    signal lcd_enable    : std_logic;
+    signal i_lcd_bus     : std_logic_vector(9 downto 0);
+    signal i_busy        : std_logic;
+    signal current_state : state_t := reset_st;
+    signal my_word       : vector_array_t;
 
 begin
 
-lcd_controller_i : lcd_controller
-port map (
-  clk        => clk,
-  reset_n    => reset_n,
-  lcd_enable => lcd_enable,
-  lcd_bus    => i_lcd_bus,
-  busy       => busy,
-  e          => e,
-  lcd_data   => lcd_data
-);
+    lcd_controller_i : lcd_controller
+    	port map (
+    		clk        => clk,
+    		reset_n    => reset_n,
+    		lcd_enable => lcd_enable,
+    		lcd_bus    => i_lcd_bus,
+    		busy       => i_busy,
+    		e          => e,
+    		rw         => rw,
+    		rs         => rs,
+    		lcd_data   => lcd_data);
+
 
 state_pro : process(current_state,clk,reset_n)
     VARIABLE clk_count : INTEGER := 0;
     VARIABLE word_index : INTEGER := 0;
 begin
+
+if(clk'EVENT and clk = '1') then
 
     if( reset_n = '1') then
         current_state <= reset_st;
@@ -68,7 +72,7 @@ begin
             end if;
 
         when idle_st =>
-            if( busy = '0' and word_index < 16 ) then
+            if( i_busy = '0' and word_index < 16 ) then
                 i_lcd_bus <= my_word(word_index);
                 lcd_enable <= '1';
                 current_state <= write_st;
@@ -91,6 +95,8 @@ begin
             current_state <= idle_st ;
 
     end case;
+
+end if;
 
 end process;
 
